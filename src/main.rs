c26df6577224cf0f40e8d59f7025630efd9f2942
InfoMathias <Mathias2820@gmail.com>
2025-08-05 23:13:52 -0400

    respond with 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
 
@@ -7,12 +10,28 @@ fn main() {
 
     for stream in listener.incoming() {
          match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
+             Ok(ref _stream) => {
+                println!("accepted new connection");
+                let stream = stream.unwrap();
+                handle_connection(stream);
              }
              Err(e) => {
                  println!("error: {}", e);
              }
          }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    //let buf_reader = BufReader::new(&stream);
+    //let http_request: Vec<_> = buf_reader
+    //    .lines()
+    //    .map(|result| result.unwrap())
+    //    .take_while(|line| !line.is_empty())
+    //    .collect();
+    //
+    //println!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }

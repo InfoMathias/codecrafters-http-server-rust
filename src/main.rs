dc2abd2f1cbc577d5b81a12c4c8db24f0a518a24
InfoMathias <Mathias2820@gmail.com>
2025-08-07 14:17:21 -0400

    return 404 for unknown routes

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,32 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    //let buf_reader = BufReader::new(&stream);
-    //let http_request: Vec<_> = buf_reader
-    //    .lines()
-    //    .map(|result| result.unwrap())
-    //    .take_while(|line| !line.is_empty())
-    //    .collect();
-    //
-    //println!("Request: {http_request:#?}");
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    
+    let mut endpoint: String = String::new();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    if !http_request[0].is_empty() {
+        endpoint = http_request[0]
+            .split(" ")
+            .nth(1)
+            .unwrap_or_default()
+            .to_string()
+            .split("/")
+            .nth(1)
+            .unwrap_or_default()
+            .to_string();
+    }
+
+    let mut response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    if endpoint != "" {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
     stream.write_all(response.as_bytes()).unwrap();
 }
